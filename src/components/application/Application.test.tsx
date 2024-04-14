7893b23646d27a7342c6fc03d52bfc7dab51e196
Naincy31 <iamnaincyrathore@gmail.com>
2024-04-14 11:55:50 +0530

    Testing the rendering and unmounting of dynamic elements

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -6,8 +6,13 @@ describe('Application', () => {
         render(<Application/>)
 
         const labelElement = screen.getByLabelText('Name')
-
         expect(labelElement).toBeInTheDocument()
+
+        const paragraphElement = screen.getByText('all', {exact: false})
+        expect(paragraphElement).toBeInTheDocument()
+
+        const paragraphEle = screen.getByText((content) => content.startsWith('All'))
+        expect(paragraphEle).toBeInTheDocument()
 
         const pageHeading = screen.getByRole('heading', {
             level: 1
